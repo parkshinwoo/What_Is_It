6fc4e5376fb24669cab17f9fa123e26d44cfeaf1
parkshinwoo1216 <parkshinwoo1216@gmail.com>
2019-04-07 15:45:09 +0900

    2019년 4월 7일 박신우 3차 커밋
    하단 네비게이션 메뉴바 수정

--- app/src/main/java/e/pshkh/what_is_it/MainActivity.kt
+++ app/src/main/java/e/pshkh/what_is_it/MainActivity.kt
@@ -2,11 +2,14 @@ package e.pshkh.what_is_it
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.Toast
 import e.pshkh.what_is_it.navigation_activity.TeacherActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,84 +29,54 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
         // 사용자가 네비게이션 바의 특정 아이템을 선택하면 해당하는 화면으로 이동시켜주는 기능입니다.
 
         when (item.itemId) {
-            /*
-            R.id.action_home -> {
 
-                /*
-                val homeFragment = HomeFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, homeFragment).commit()
-                */
+            R.id.action_teacher -> {
 
-                return true
-            }
-
-              R.id.recognize_text -> {
-
-
-
-                return true
-
-            }
-
-            R.id.recognize_face -> {
-
-
-
-                return true
-
-            }
-
-            R.id.action_image -> {
-
-                // 스토리지 접근 권한 체크를 합니다.
+                // 스토리지, 카메라 접근 권한 체크를 합니다.
                 if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
                     PackageManager.PERMISSION_GRANTED) {
-                    startActivity(Intent(this, UploadPhotoActivity::class.java))
+                    startActivity(Intent(this, TeacherActivity::class.java))
                 } else {
                     Toast.makeText(this, "EXTERNAL STORAGE 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
                 }
 
-
                 return true
             }
 
-            R.id.landmark -> {
-
+            R.id.action_diary -> {
 
             }
-            R.id.language -> {
 
+            R.id.action_recognize_emotion -> {
 
+                /*
+                // 스토리지, 카메라 접근 권한 체크를 합니다.
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, EmotionActivity::class.java))
+                } else {
+                    Toast.makeText(this, "EXTERNAL STORAGE 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
+                }
+                */
 
-                return true
-            }
-            */
-
-            R.id.action_teacher -> {
-
-                startActivity(Intent(this, TeacherActivity::class.java))
-
-                return true
             }
 
         }
         return false
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_navigation_top.setOnNavigationItemSelectedListener(this)
-        bottom_navigation_middle.setOnNavigationItemSelectedListener(this)
-        bottom_navigation_down.setOnNavigationItemSelectedListener(this)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
 
-        // 시작할때마다 홈에서 시작하게끔 하단 네비게이션의 현재 선택된 아이템을 "home"으로 지정합니다.
-        bottom_navigation_top.selectedItemId = R.id.action_home
+        // 시작할때마다 홈에서 시작하게끔 하단 네비게이션의 현재 선택된 아이템을 다이어리로 지정합니다.
+        bottom_navigation.selectedItemId = R.id.action_diary
 
-        // 디바이스 사진첩에 접근할 권한을 줍니다.
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1 )
+        // 디바이스 사진첩, 카메라에 접근할 권한을 줍니다.
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA),1 )
     }
 }
